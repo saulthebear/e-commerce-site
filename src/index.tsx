ea57f382dc923a0a08a9e74b55f8da90a9d4430a
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-17 13:44:32 -0600

    Install Tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 import App from './App';
 
