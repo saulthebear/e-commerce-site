1e402f19476b2f354e00dd910d63c756ed52855a
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-18 12:26:30 -0600

    Add API calls for categories

---
+++ src/API/categories.ts
@@ -0,0 +1,46 @@
+import config from '../config/config';
+import { ICategoryBody } from '../interfaces/product';
+
+export const createCategory = async (body: ICategoryBody, token: string) => {
+  const response = await fetch(`${config.server.url}/categories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  });
+  return response.json();
+};
+
+export const getCategories = async () => {
+  const response = await fetch(`${config.server.url}/categories`);
+  return response.json();
+};
+
+export const updateCategory = async (
+  id: string,
+  body: Partial<ICategoryBody>,
+  token: string
+) => {
+  const response = await fetch(`${config.server.url}/categories/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(body),
+  });
+  return response.json();
+};
+
+export const deleteCategory = async (id: string, token: string) => {
+  const response = await fetch(`${config.server.url}/categories/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.json();
+};
