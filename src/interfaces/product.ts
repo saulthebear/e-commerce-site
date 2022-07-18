575fa867683d7e332c7cd7a1e670451fd534d737
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-17 18:17:07 -0600

    Add category interface

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -1,10 +1,22 @@
+export interface ICategoryBody {
+  title: string;
+  description: string;
+}
+
+export interface ICategoryDocument extends ICategoryBody {
+  _id: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 export interface IProductBody {
   title: string;
   description: string;
   price: number;
   category: string;
+  image_url?: string;
 }
 
-export interface IProductDocument extends IProductBody {
+export interface IProductDocument extends Omit<IProductBody, 'category'> {
   _id: string;
+  category: ICategoryDocument;
 }
