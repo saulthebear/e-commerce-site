f1e2b46ae0c2663209d00725c17a6015087b9f4e
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-19 17:14:58 -0600

    Change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,9 @@ module.exports = {
     extend: {
       fontFamily: {
         baloo: ['"Baloo 2"', 'cursive'],
+        obviously: ['Obviously', 'sans-serif'],
         brand: ['Vollkorn', 'serif'],
-        sans: ['"Baloo 2"', 'sans-serif'],
+        sans: ['Obviously', 'sans-serif'],
       },
       spacing: {
         'navbar-height': '64px',
