1c46d1125fb7a0e541a94f65427ad897f237ddb9
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-19 22:15:07 -0600

    Style checkout success page

--- src/pages/CheckoutSuccess.tsx
+++ src/pages/CheckoutSuccess.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PageHead from '../components/UI/PageHead';
+import { MdNavigateBefore } from 'react-icons/md';
 
 const CheckoutSuccess = () => {
   return (
-    <div>
-      <div>Success!</div>
+    <div className="w-full text-center">
+      <PageHead title="Success!" />
+      <div className="w-full text-center">Your order has been placed.</div>
+      <div className="flex justify-center items-center mt-5">
+        <Link
+          to="/orders"
+          className="hover:border-b-2 border-orange-700 uppercase text-sm font-medium flex text-orange-700 w-fit"
+        >
+          <MdNavigateBefore className="fill-orange-700 text-2xl" />
+          View your orders
+        </Link>
+      </div>
     </div>
   );
 };
