6a0b71b4005286917717fac6ff6130de8675caf3
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-19 21:17:34 -0600

    Style cart

--- src/components/Navbar/Cart.tsx
+++ src/components/Navbar/Cart.tsx
@@ -5,11 +5,14 @@ import {
   checkout,
   clearCart,
   decrementItem as decrementItemAPI,
+  removeItem as removeItemAPI,
 } from '../../API/cart';
 import logging from '../../config/logging';
 import UserContext from '../../contexts/user';
 import { dbPriceToClientPriceString } from '../../utils/priceFunctions';
+import Button from '../UI/Button';
 import Loading from '../UI/Loading';
+import { MdRemoveShoppingCart } from 'react-icons/md';
 
 export interface ICartProps {
   isOpen: boolean;
@@ -30,28 +33,49 @@ const Cart: React.FC<ICartProps> = ({ isOpen, setIsOpen, toggleCart }) => {
         <img
           src={product.image_url}
           alt={product.title}
-          className="max-w-[75px]"
+          className="w-28 object-cover"
         />
         <div className="flex flex-col justify-center ml-2">
-          <div className="">
-            <p className="text-sm">{product.title}</p>
-            <p className="text-sm">
-              {dbPriceToClientPriceString(product.price)}
-            </p>
-          </div>
-          <div className="flex border-[1px] w-fit border-slate-700">
-            <button onClick={() => decrementItem(product._id)}>
-              <MdRemove />
-            </button>
-            <div className="text-sm">{item.quantity}</div>
-            <button onClick={() => incrementItem(product._id)}>
-              <MdAdd />
-            </button>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">{product.title}</p>
+            <div className="flex justify-between items-center">
+              <p className="text-sm">
+                {dbPriceToClientPriceString(product.price)}
+              </p>
+              <button
+                className="hover:bg-slate-200 rounded-full"
+                onClick={() => removeItem(product._id)}
+              >
+                <MdRemoveShoppingCart size="1.2rem" />
+              </button>
+            </div>
+            <div className="flex justify-between p-2 rounded-sm border-[1px] border-slate-700">
+              <button onClick={() => decrementItem(product._id)}>
+                <MdRemove />
+              </button>
+              <div className="text-sm">{item.quantity}</div>
+              <button onClick={() => incrementItem(product._id)}>
+                <MdAdd />
+              </button>
+            </div>
           </div>
         </div>
       </div>
     );
   });
+
+  const removeItem = async (product_id: string) => {
+    setIsLoading(true);
+    const newCart = await removeItemAPI(product_id, cart, userState.fire_token);
+    userDispatch({
+      type: 'SET_CART',
+      payload: {
+        user: { ...userState.user, cart: newCart },
+        fire_token: userState.fire_token,
+      },
+    });
+    setIsLoading(false);
+  };
 
   const incrementItem = async (product_id: string) => {
     setIsLoading(true);
@@ -129,15 +153,29 @@ const Cart: React.FC<ICartProps> = ({ isOpen, setIsOpen, toggleCart }) => {
             <MdClose size="1.5em" />
           </button>
         </div>
-        {isLoading ? <Loading>Loading cart...</Loading> : <>{cart_items}</>}
-        <div>
-          <p className="text-sm">
-            Total: {dbPriceToClientPriceString(totalPrice)}
-          </p>
-          <button onClick={handleCheckout}>
-            <p className="text-sm">Checkout</p>
-          </button>
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className="flex flex-col gap-5">{cart_items}</div>
+        )}
+        {cart.items.length === 0 && (
+          <div className="text-center text-sm">
+            <p>Your cart is empty</p>
+          </div>
+        )}
+        {cart.items.length > 0 && (
+          <div className="mt-10">
+            <p className="font-bold w-full text-center mb-3 text-lg">
+              Total: {dbPriceToClientPriceString(totalPrice)}
+            </p>
+            <Button
+              className="bg-emerald-600 hover:bg-emerald-700 text-white w-full"
+              onClick={handleCheckout}
+            >
+              Checkout
+            </Button>
+          </div>
+        )}
       </div>
       {/* backdrop */}
       {isOpen && (
