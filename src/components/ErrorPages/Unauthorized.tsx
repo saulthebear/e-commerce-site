d844ac7b3d9f4d3ca038a75860441d6554a9b54b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-19 21:39:47 -0600

    Style unauthorized page

--- src/components/ErrorPages/Unauthorized.tsx
+++ src/components/ErrorPages/Unauthorized.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PageHead from '../UI/PageHead';
+import { MdNavigateBefore } from 'react-icons/md';
 
 const Unauthorized = () => {
   return (
-    <div>
-      <h1>Unauthorized</h1>
-      <p>You do not have permission to access this page.</p>
+    <div className="bg-red-700 text-white p-10 rounded-lg">
+      <PageHead title="Unauthorized" />
+      <p className="text-center">
+        You do not have permission to access this page.
+      </p>
+      <Link to="/" className="flex justify-center items-center mt-10 ">
+        <MdNavigateBefore className="fill-white text-2xl" />
+        Home
+      </Link>
     </div>
   );
 };
